37151256aaec90a51ba1f0ef6da004213c48aa06
Jinseo Park <jason.park@gatech.edu>
2020-02-08 11:18:03 -0500

    Updates

--- precache-manifest.3503ff8bf61d477b6093f27f2bbc9d1f.js
+++ precache-manifest.3503ff8bf61d477b6093f27f2bbc9d1f.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "cd745e5c732f767ef1b9c21f392bd768",
+    "revision": "ec07edc1bcc6651bcde15163657d707d",
     "url": "/capture-the-flag/index.html"
   },
   {
-    "revision": "5002e7312b94aff1802d",
+    "revision": "0374664a345e16c5918d",
     "url": "/capture-the-flag/static/css/main.22f82358.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/capture-the-flag/static/js/2.362b9b5b.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "5002e7312b94aff1802d",
-    "url": "/capture-the-flag/static/js/main.a634bc5e.chunk.js"
+    "revision": "0374664a345e16c5918d",
+    "url": "/capture-the-flag/static/js/main.339f48d4.chunk.js"
   },
   {
     "revision": "086c8c286cf8f12298b6",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/capture-the-flag/precache-manifest.326b8cbd52f442e3bcf0517f4659b476.js"
+  "/capture-the-flag/precache-manifest.3503ff8bf61d477b6093f27f2bbc9d1f.js"
 );
 
 self.addEventListener('message', (event) => {
